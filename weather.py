fa8bec528c4d354a7e6ab8af4d10c4fefd9943ba
Ka Tai Ho <kataihoo@gmail.com>
2017-07-27 23:47:00 -0700

    adding ui stuff for the app

--- weather.py
+++ weather.py
@@ -3,11 +3,12 @@ kivy.require('1.10.0')
 
 from kivy.app import App 
 from kivy.uix.button import Label
+from kivy.uix.floatlayout import FloatLayout
 
 class WeatherApp(App):
 
 	def build(self):
-		return Label()
+		return FloatLayout()
 
 if __name__ == "__main__":
 	weather_kivy = WeatherApp()
