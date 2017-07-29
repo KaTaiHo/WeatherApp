963dd22c57ffa39a9eee3d1aa34757fc0abc3088
Ka Tai Ho <kataihoo@gmail.com>
2017-07-28 22:29:40 -0700

    working with API and Labels

--- app/weather.py
+++ app/weather.py
@@ -6,14 +6,29 @@ kivy.require('1.10.0')
 from kivy.app import App 
 from kivy.uix.button import Label
 from kivy.uix.floatlayout import FloatLayout
+from kivy.properties import StringProperty
+from kivy.core.window import Window
+Window.size = (400, 700)
 
 
 class WeatherFloatLayout(FloatLayout):
 	
+	data = {}
+	current_description = StringProperty()
+	temp = 0.0
+
+	def __init__(self, **kwargs):
+		super(WeatherFloatLayout, self).__init__(**kwargs)
+		self.data = ''
+
 	def get_weather_data(self):
+		print ('data pressed')
 		api_key = '28d0ca9a9aff408aa4f90c51689be4ed'
 		r = requests.get('http://api.openweathermap.org/data/2.5/weather?zip=78751,us&appid=' + api_key)
 		self.data = json.loads(r.text)
+		self.current_description = self.data['weather'][0]['description']
+		self.temp = self.data['main']['temp']
+
 
 
 
